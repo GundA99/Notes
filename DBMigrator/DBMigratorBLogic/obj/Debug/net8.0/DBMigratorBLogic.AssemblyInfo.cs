fd6d156df7095274581e9ed3e80143c2c63ab9c9
GundA99 <akashgund1999@gmail.com>
2025-07-17 10:31:20 +0530

    1707

--- DBMigrator/DBMigratorBLogic/obj/Debug/net8.0/DBMigratorBLogic.AssemblyInfo.cs
+++ DBMigrator/DBMigratorBLogic/obj/Debug/net8.0/DBMigratorBLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBMigratorBLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801f03199747dd5eaf8af83ea99ef0c7ea1da077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0016d1e06c6ca2ecdfb537d38952a18227d87dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBMigratorBLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBMigratorBLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
